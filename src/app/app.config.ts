ce3711218e26e1e9ad8776bb0765fae44f497d91
Shailesh <skrock280@gmail.com>
2025-03-30 11:58:44 +0530

    add hashlocation routing stategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),{
-    provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-    useValue: { appearance: 'outline', subscriptSizing: 'dynamic' }
-  },]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline', subscriptSizing: 'dynamic' },
+    },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ],
 };
